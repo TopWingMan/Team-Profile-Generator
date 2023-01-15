10527adfb759efad9695f5d8ef032b54176c546f
Zachary Stanaford <ZacharyStanaford@gmail.com>
2023-01-14 18:29:55 -0800

    js.index can now create a html file

--- index.js
+++ index.js
@@ -72,3 +72,58 @@ const internQuestions =
         name:"internGithub"
     }
 ]
+
+inquirer
+    .prompt(managerQuestions)
+    .then((data) =>
+    {
+        writeToFile();
+    })
+
+function writeToFile()
+{
+    fs.writeFile
+        ('new.html',
+            `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                <link rel="stylesheet" href="./style.css">
+            </head>
+            <body>
+                <div style="background-color: rgb(252, 102, 102); padding: 2%;">
+                    <h1 style="text-align: center; color: white;">My Team</h1>
+                </div>
+            
+                <div id="CardHolder">
+            
+                    <div style="padding: 10px;">
+                        <div class="Card">
+                            <div class="CardTop">
+                                <p class="CardText">Jared <br> $ Title</p>
+                            </div>
+                    
+                            <div class="CardBottom">
+                                <p class="BottomText">ID: 1</p>
+                                <p class="BottomText">Email: ford@gmail.com</p>
+                                <p class="BottomText">Office number: 1</p>
+                            </div>
+                        </div>
+                    </div>
+            
+                </div>
+            
+                </body>
+            </body>
+            </html>`
+
+            , function (err) 
+            {
+                if (err) throw err;
+                console.log('CreatedFile!');
+            }
+        );
+}
